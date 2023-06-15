04b9a4ffeafa302d7ccce3698c78dd815be9fd76
Asif Kabir <122868038+AsifKabir1234@users.noreply.github.com>
2023-06-16 00:11:09 +0600

    Update models.py

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -13,7 +13,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
-        class Contact(models.Model):
+    
+ class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=70)
     email = models.CharField(max_length=70, default="")
